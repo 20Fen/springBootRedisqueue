df103868f41bad75b1ff75fbe51c7d17715d232d
20fen <15622969639@163.com>
2019-11-27 14:34:31 +0800

    提交

--- src/main/java/com/example/demo/production/Producer.java
+++ src/main/java/com/example/demo/production/Producer.java
@@ -1,8 +1,6 @@
 package com.example.demo.production;
 
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.example.demo.model.Test;
 import com.example.demo.util.JedisUtil;
 import com.example.demo.util.ObjectUtil;
@@ -10,7 +8,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * Description:
+ * Description: 测试
  */
 @Component
 @RequestMapping("/in")
